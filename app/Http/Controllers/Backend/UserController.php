8644e96420e959563528e1256213c10cf1fe1a05
haniful360 <haniful360@gmail.com>
2024-10-21 14:44:45 +0600

    user data show

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -10,10 +10,10 @@ use function Pest\Laravel\json;
 
 class UserController extends Controller
 {
-    public function userView(){
+    public function userView()
+    {
         // $allData = User::all();
         $data['allData'] = User::all();
-        return view('backend.user.user_view',$data);
-        // return response()->json(['message' => 'view user']);
+        return view('backend.user.user_view', $data);
     }
 }
